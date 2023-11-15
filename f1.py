66f5720ea6899697d53eddf8f2fc1608abf8e646
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-15 17:43:59 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -2,10 +2,10 @@ import paramiko
 import difflib
 import time
 
+# Define router credentials and IP
 router_ip = '192.168.56.30'
 router_username = 'cisco'
 router_password = 'cisco123!'
-syslog_server_ip = '192.168.56.101'  # Replace with the syslog server IP
 
 # Function to fetch router configuration via SSH
 def fetch_config():
@@ -14,6 +14,7 @@ def fetch_config():
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(router_ip, username=router_username, password=router_password)
 
+        # Sending commands to fetch running configuration
         ssh_shell = ssh_client.invoke_shell()
         ssh_shell.send('terminal length 0\n')
         ssh_shell.send('show running-config\n')
@@ -26,43 +27,62 @@ def fetch_config():
         print(f"Failed to fetch configuration: {e}")
         return None
 
-# Fetch running and startup configs
-running_config = fetch_config()
-if running_config:
-    with open('running_config.txt', 'w') as file:
-        file.write(running_config)
-    print("Running config saved to 'running_config.txt'.")
+# Function to compare running config with startup config
+def compare_with_startup_config():
+    # Read startup configuration file
+    try:
+        with open('startup_config.txt', 'r') as file:
+            startup_config = file.read()
 
-# Load stored offline config
-try:
-    with open('stored_offline_config.txt', 'r') as file:
-        stored_offline_config = file.read()
+        if startup_config:
+            # Compare running with startup configuration
+            diff = difflib.unified_diff(running_config.splitlines(), startup_config.splitlines())
+            differences_found = False
+            print("\ni. Differences between running config and startup config:")
+            for line in diff:
+                differences_found = True
+                print(line)
+            if not differences_found:
+                print("No differences found between running config and startup config.")
+        else:
+            print("Startup config file not found.")
+    except FileNotFoundError:
+        print("Startup config file not found.")
 
-    # Compare running with stored offline config
-    if stored_offline_config:
-        diff = difflib.unified_diff(running_config.splitlines(), stored_offline_config.splitlines())
-        differences_found = False
-        print("Differences between running config and stored offline config:")
-        for line in diff:
-            differences_found = True
-            print(line)
-        if not differences_found:
-            print("No differences found between running config and stored offline config.")
-    else:
-        print("Stored offline config not found.")
-except FileNotFoundError:
-    print("Stored offline config file not found.")
+# Function to compare running config with local offline config
+def compare_with_local_offline_config(running_config):
+    # Read local offline configuration file
+    try:
+        with open('stored_offline_config.txt', 'r') as file:
+            stored_offline_config = file.read()
+
+        if stored_offline_config:
+            # Compare running with stored offline configuration
+            diff = difflib.unified_diff(running_config.splitlines(), stored_offline_config.splitlines())
+            differences_found = False
+            print("\nii. Differences between running config and local offline config:")
+            for line in diff:
+                differences_found = True
+                print(line)
+            if not differences_found:
+                print("No differences found between running config and local offline config.")
+        else:
+            print("Local offline config file not found.")
+    except FileNotFoundError:
+        print("Local offline config file not found.")
 
 # Function to compare running config against hardening advice
-def compare_with_hardening_advice():
+def compare_with_hardening_advice(running_config):
+    # Read hardening advice file
     try:
         with open('hardening_advice.txt', 'r') as file:
             hardening_advice = file.read()
 
         if hardening_advice:
+            # Compare running with hardening advice
             diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
             differences_found = False
-            print("Differences between running config and hardening advice:")
+            print("\niii. Differences between running config and hardening advice:")
             for line in diff:
                 differences_found = True
                 print(line)
@@ -85,18 +105,20 @@ def configure_syslog():
         ssh_shell.send('cisco123!\n')
         time.sleep(1)
         ssh_shell.send('conf t\n')
-        ssh_shell.send(f'logging {syslog_server_ip}\n')  # Replace with actual syslog server IP
+        ssh_shell.send('logging 192.168.56.101\n')  # Replace with actual syslog server IP
         time.sleep(1)
         ssh_shell.send('end\n')
         ssh_shell.send('write memory\n')
         ssh_client.close()
 
-        print(f"Syslog configuration for {syslog_server_ip} completed successfully.")
+        print("\niv. Syslog configuration completed successfully.")
     except Exception as e:
         print(f"Failed to configure syslog: {e}")
 
-# Compare running config against hardening advice
-compare_with_hardening_advice()
-
-# Configure syslog on the router
-configure_syslog()
+# Fetch running configuration
+running_config = fetch_config()
+if running_config:
+    compare_with_startup_config()
+    compare_with_local_offline_config(running_config)
+    compare_with_hardening_advice(running_config)
+    configure_syslog()
