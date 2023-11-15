062f9926cf8a5b46d9d82680b8107ac6b4b9115f
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-15 14:28:34 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,6 +1,39 @@
-import paramiko
+def apply_hardening(ip, username, password, enable_password):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=username, password=password)
 
-# ... (previous code remains unchanged)
+        # Enter enable mode
+        ssh_shell = ssh.invoke_shell()
+        ssh_shell.send("enable\n")
+        ssh_shell.send(enable_password + "\n")
+        output = ssh_shell.recv(65535).decode('utf-8')
+
+        # Configuration commands for hardening
+        commands = [
+            "conf t",
+            "ip access-list standard SSH-ACL",
+            "permit <your_admin_IP>",  # Replace <your_admin_IP> with your actual IP
+            "exit",
+            "line vty 0 15",
+            "access-class SSH-ACL in",
+            "transport input ssh",
+            # Other hardening commands as needed
+            "end",
+            "write memory"  # Save configuration
+        ]
+
+        for command in commands:
+            ssh_shell.send(command + "\n")
+            output = ssh_shell.recv(65535).decode('utf-8')
+
+        ssh.close()
+        print("Hardening configuration applied successfully.")
+    except Exception as e:
+        print(f"Failed to apply hardening: {e}")
+
+
 
 def configure_syslog(ip, username, password, enable_password):
     try:
@@ -61,5 +94,3 @@ def configure_event_logging(ip, username, password, enable_password):
         print("Event logging configuration completed successfully.")
     except Exception as e:
         print(f"Failed to configure event logging: {e}")
-
-# ... (remaining code remains unchanged)
