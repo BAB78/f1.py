27a5fe4cccd8da3d9f7a5e200e03dbdca2fbe52c
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-15 17:56:28 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -6,88 +6,92 @@ router_ip = '192.168.56.30'
 router_username = 'cisco'
 router_password = 'cisco123!'
 
-# Function to fetch router configuration via SSH
-def fetch_config():
+# Function to establish an SSH connection
+def establish_connection():
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(router_ip, username=router_username, password=router_password)
+        return ssh_client
+    except Exception as e:
+        print(f"Failed to establish SSH connection: {e}")
+        return None
 
-        ssh_shell = ssh_client.invoke_shell()
-        ssh_shell.send('terminal length 0\n')
-        ssh_shell.send('show running-config\n')
-        time.sleep(2)
-        running_config = ssh_shell.recv(65535).decode('utf-8')
-
-        ssh_client.close()
-        return running_config
+# Function to fetch router configuration via SSH
+def fetch_config():
+    try:
+        ssh_client = establish_connection()
+        if ssh_client:
+            ssh_shell = ssh_client.invoke_shell()
+            ssh_shell.send('terminal length 0\n')
+            time.sleep(1)
+            ssh_shell.send('show running-config\n')
+            time.sleep(2)
+            running_config = ssh_shell.recv(65535).decode('utf-8')
+            ssh_client.close()
+            return running_config
     except Exception as e:
         print(f"Failed to fetch configuration: {e}")
         return None
 
-# Fetch running and startup configs
+# Function to compare configurations
+def compare_configs(config1, config2, title):
+    if config1 and config2:
+        diff = difflib.unified_diff(config1.splitlines(), config2.splitlines())
+        print(f"Differences between {title}:")
+        for line in diff:
+            print(line)
+    else:
+        print(f"Failed to compare {title}.")
+
+# Fetch running configuration
 running_config = fetch_config()
 if running_config:
     with open('running_config.txt', 'w') as file:
         file.write(running_config)
-    
-    print("Comparing running configuration with startup configuration:")
-    # Load stored offline config for comparison
+    print("Running configuration fetched successfully.")
+else:
+    print("Failed to fetch running configuration.")
+
+# Load stored offline config
+with open('stored_offline_config.txt', 'r') as file:
+    stored_offline_config = file.read()
+
+# Compare running with stored offline config
+compare_configs(running_config, stored_offline_config, "running and stored offline configuration")
+
+# Function to compare running config against hardening advice
+def compare_with_hardening_advice():
+    # Load hardening advice from a file or Moodle page
+    with open('hardening_advice.txt', 'r') as file:
+        hardening_advice = file.read()
+
+    compare_configs(running_config, hardening_advice, "running configuration and hardening advice")
+
+# Configure syslog on the router
+def configure_syslog():
     try:
-        with open('stored_offline_config.txt', 'r') as file:
-            stored_offline_config = file.read()
-
-        # Compare running with stored offline config
-        if stored_offline_config:
-            diff = difflib.unified_diff(running_config.splitlines(), stored_offline_config.splitlines())
-            print("Differences between running config and stored offline config:")
-            for line in diff:
-                print(line)
-        else:
-            print("Stored offline configuration not found.")
-    except FileNotFoundError:
-        print("Stored offline configuration file not found.")
-
-    # Function to compare running config against hardening advice
-    def compare_with_hardening_advice():
-        # Load hardening advice from a file or Moodle page
-        try:
-            with open('hardening_advice.txt', 'r') as file:
-                hardening_advice = file.read()
-
-            if hardening_advice:
-                print("Comparing running configuration with Cisco device hardening advice:")
-                diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
-                for line in diff:
-                    print(line)
-            else:
-                print("Hardening advice not found.")
-        except FileNotFoundError:
-            print("Hardening advice file not found.")
-
-    # Function to configure syslog on the router
-    def configure_syslog():
-        try:
-            ssh_client = paramiko.SSHClient()
-            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(router_ip, username=router_username, password=router_password)
-
+        ssh_client = establish_connection()
+        if ssh_client:
             ssh_shell = ssh_client.invoke_shell()
             ssh_shell.send('enable\n')
             ssh_shell.send('cisco123!\n')
             time.sleep(1)
-            ssh_shell.send(f'conf t\nlogging {router_ip}\n')
+            ssh_shell.send('conf t\n')
+            ssh_shell.send('logging <syslog_server_ip>\n')  # Replace with actual syslog server IP
             time.sleep(1)
-            ssh_shell.send('end\nwrite memory\n')
+            ssh_shell.send('end\n')
+            ssh_shell.send('write memory\n')
             ssh_client.close()
 
             print("Syslog configuration completed successfully.")
-        except Exception as e:
-            print(f"Failed to configure syslog: {e}")
+        else:
+            print("Failed to establish SSH connection for syslog configuration.")
+    except Exception as e:
+        print(f"Failed to configure syslog: {e}")
 
-    # Compare running config against hardening advice
-    compare_with_hardening_advice()
+# Compare running config against hardening advice
+compare_with_hardening_advice()
 
-    # Configure syslog on the router
-    print("Enabling syslog for event logging and monitoring:")
-    configure_syslog()
+# Configure syslog on the router
+configure_syslog()
