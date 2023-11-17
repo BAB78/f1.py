173fa800280a4fe20a2d44e0a91b5a4045dd509d
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-17 02:57:21 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,17 +1,3 @@
-Enter your choice (1-3): 1       
-Comparing running config with hardening advice...
-Traceback (most recent call last):
-  File "task.py", line 107, in <module>
-    display_menu()
-  File "task.py", line 98, in display_menu
-    compare_with_hardening_advice()
-  File "task.py", line 45, in compare_with_hardening_advice
-    with open('hardening_advice.txt', 'r') as f:
-FileNotFoundError: [Errno 2] No such file or directory: 'hardening_advice.txt'
-
-
-
-
 import telnetlib
 import difflib
 
@@ -19,7 +5,10 @@ import difflib
 ip_address = '192.168.56.101'
 username = 'cisco'
 password = 'cisco123!'
-enable_password = 'cisco123!'
+enable_password = 'class123!'
+output_file = 'running_config.txt'  # Name of the local file to save the running configuration
+offline_config_file = 'startup_config.txt'  # Path to save the startup configuration
+running_config_telnet = None
 
 # Function to handle Telnet login and command execution
 def telnet_session(ip, user, passwd, enable_pass, command):
@@ -29,8 +18,6 @@ def telnet_session(ip, user, passwd, enable_pass, command):
         tn.write(user.encode('utf-8') + b'\n')
         tn.read_until(b'Password: ', timeout=10)
         tn.write(passwd.encode('utf-8') + b'\n')
-        tn.read_until(b'>', timeout=10)
-        tn.write(b'enable\n')
         tn.read_until(b'Password: ', timeout=10)
         tn.write(enable_pass.encode('utf-8') + b'\n')
 
@@ -54,49 +41,27 @@ def telnet_session(ip, user, passwd, enable_pass, command):
 
 # Function to compare with hardening advice 
 def compare_with_hardening_advice():
-    print("Comparing running config with hardening advice...")
-    
-    with open('hardening_advice.txt', 'r') as f:
-        hardening_advice = f.read()
+    try:
+        with open('configs/hardening_advice.txt', 'r') as f:
+            hardening_advice = f.read()
 
-    running_config = telnet_session(ip_address, username, password, enable_password, 'show running-config')
+        running_config = telnet_session(ip_address, username, password, enable_password, 'show running-config')
 
-    if running_config:
-        diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
-        diff_result = '\n'.join(diff)
-        if len(diff_result) > 0:
-            print("Differences found with hardening advice:")
-            print(diff_result)
+        if running_config:
+            diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
+            diff_result = '\n'.join(diff)
+            if len(diff_result) > 0:
+                print(diff_result)
+            else:
+                print("No differences found with hardening advice.")
         else:
-            print("No differences found with hardening advice.")
-    else:
-        print("Failed to retrieve the running configuration.")
+            print("Failed to retrieve the running configuration.")
+    except FileNotFoundError:
+        print("Hardening advice file not found. Please check the file path.")
 
 # Function to configure syslog
 def configure_syslog():
-    try:
-        tn = telnetlib.Telnet(ip_address)
-        tn.read_until(b'Username: ', timeout=10)
-        tn.write(username.encode('utf-8') + b'\n')
-        tn.read_until(b'Password: ', timeout=10)
-        tn.write(password.encode('utf-8') + b'\n')
-        tn.read_until(b'>', timeout=10)
-        tn.write(b'enable\n')
-        tn.read_until(b'Password: ', timeout=10)
-        tn.write(enable_password.encode('utf-8') + b'\n')
-        
-        tn.write(b'configure terminal\n')
-        tn.read_until(b'#', timeout=10)
-        tn.write(b'logging 192.168.1.100\n')  # Replace with your syslog server IP
-        tn.read_until(b'#', timeout=10)
-        tn.write(b'end\n')
-        tn.read_until(b'#', timeout=10)
-        tn.write(b'write memory\n')
-        
-        print("Syslog configured successfully for event logging and monitoring.")
-        tn.close()
-    except Exception as e:
-        print(f"Error configuring syslog: {e}")
+    # Your syslog configuration logic here
 
 # Function to display menu and execute selected option
 def display_menu():
