a599c35fc15f8f0246c2d711d7424a1b827cad8e
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-17 20:45:18 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -100,11 +100,11 @@ display_menu()
 
 
 
-Apologies for the inconvenience. Let's modify the code to handle the "Hardening advice file not found" error correctly:
 
-```python
+
 import telnetlib
 import difflib
+import os
 
 # Define common variables
 router_ip_address = '192.168.56.101'
@@ -112,6 +112,7 @@ router_username = 'cisco'
 router_password = 'cisco123!'
 enable_password = 'class123!'
 hardening_advice_file_path = 'hardening_advice.txt'  # Path to the hardening advice file
+
 
 # Function to establish a Telnet session and execute commands
 def telnet_session(ip, username, password, enable_password, commands):
@@ -142,36 +143,35 @@ def telnet_session(ip, username, password, enable_password, commands):
         print(f'Telnet Session Failed: {e}')
         return None
 
+
 # Function to compare running configuration with hardening advice
 def compare_running_config_with_hardening_advice():
-    try:
-        # Read hardening advice from file
-        with open(hardening_advice_file_path, 'r', encoding='utf-8') as f:
-            hardening_advice = f.read()
+    # Check if hardening advice file exists
+    if not os.path.exists(hardening_advice_file_path):
+        print(f"Hardening advice file not found: {hardening_advice_file_path}")
+        return
 
-        # Retrieve the running configuration
-        commands = ['show running-config']
-        running_config = telnet_session(router_ip_address, router_username, router_password, enable_password, commands)
+    # Read hardening advice from file
+    with open(hardening_advice_file_path, 'r', encoding='utf-8') as f:
+        hardening_advice = f.read()
 
-        if running_config:
-            # Compare the running configuration to hardening advice
-            diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
-            diff_result = '\n'.join(diff)
+    # Retrieve the running configuration
+    commands = ['show running-config']
+    running_config = telnet_session(router_ip_address, router_username, router_password, enable_password, commands)
 
-            if len(diff_result) > 0:
-                print("Differences found with hardening advice:")
-                print(diff_result)
-            else:
-                print("No significant differences found with hardening advice.")
+    if running_config:
+        # Compare the running configuration to hardening advice
+        diff = difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines())
+        diff_result = '\n'.join(diff)
+
+        if len(diff_result) > 0:
+            print("Differences found with hardening advice:")
+            print(diff_result)
         else:
-            print("Failed to retrieve the running configuration.")
-    except FileNotFoundError:
-        print("Hardening advice file not found. Please check the file path.")
-    except Exception as e:
-        print(f'Error comparing configurations: {e}')
+            print("No significant differences found with hardening advice.")
+    else:
+        print("Failed to retrieve the running configuration.")
+
 
 # Main execution
 compare_running_config_with_hardening_advice()
-```
-
-Ensure that the `hardening_advice.txt` file is in the same directory as the script. Running the script will now handle the "Hardening advice file not found" error appropriately. If the issue persists, please provide more details about the error you're experiencing.ecks whether the hardening advice file exists and provides an appropriate error message.
