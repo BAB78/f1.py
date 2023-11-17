8508f8600bb757c8d57540b184096ff3875f01f3
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-17 03:46:28 +0000

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,8 +1,29 @@
-2023-11-17 03:40:43,303 - ERROR - Hardening advice file not found. Please check the file path.
+def compare_with_hardening_advice():
+    try:
+        with open('configs/hardening_advice.txt', 'r') as f:
+            hardening_advice = f.read()
 
+        running_config = telnet_session(ip_address, username, password, enable_password, 'show running-config')
+
+        if running_config:
+            diff_result = list(difflib.unified_diff(running_config.splitlines(), hardening_advice.splitlines()))
+
+            if diff_result:
+                logging.info('Differences found with hardening advice:')
+                for line in diff_result:
+                    print(line)
+            else:
+                logging.info("No significant differences found with hardening advice.")
+        else:
+            logging.error("Failed to retrieve the running configuration.")
+
+    except FileNotFoundError:
+        logging.error("Hardening advice file not found. Please check the file path.")
 import telnetlib
 import difflib
 import logging
+
+
 
 # Define logger for error handling
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
